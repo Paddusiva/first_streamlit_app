587c6e0e64295c9a3b6d50beb5d49b13f994c455
Paddusiva <111980175+Paddusiva@users.noreply.github.com>
2022-08-25 21:24:41 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,3 +4,6 @@ streamlit.header('Menu')
 streamlit.text('Vegies')
 streamlit.text('green leaves')
 
+import pandas
+my fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
