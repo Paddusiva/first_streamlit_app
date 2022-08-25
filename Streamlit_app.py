2cbb755b8e75e251617b1d51aef4f97562daeca5
Paddusiva <111980175+Paddusiva@users.noreply.github.com>
2022-08-25 21:29:17 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.text('Vegies')
 streamlit.text('green leaves')
 
 import pandas
-my fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my fruit_list = pandas.read_csv ('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruit_list)
