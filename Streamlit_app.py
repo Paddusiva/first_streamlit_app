00fcc94c55ffde28cefe80dc9a0e094391768161
Paddusiva <111980175+Paddusiva@users.noreply.github.com>
2022-08-29 14:46:57 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit
+/*import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Menu')
 streamlit.text('Vegies')
@@ -12,7 +12,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list) */
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
